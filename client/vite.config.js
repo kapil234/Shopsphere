3df24fcdfe2a835385efb388bb6aeb2ca7ae4aa8
kapil234 <109575291+kapil234@users.noreply.github.com>
2026-02-24 20:14:33 +0530

    some changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,7 @@ import envCompatible from 'vite-plugin-env-compatible'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: "/",
   plugins: [react(),
      tailwindcss(),
     envCompatible()],
