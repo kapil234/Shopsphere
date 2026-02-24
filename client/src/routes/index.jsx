44dddb3f957714dbe5d6aedf05689e8aa16ee721
kapil234 <109575291+kapil234@users.noreply.github.com>
2026-02-24 20:04:16 +0530

    update route

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,83 +1,83 @@
-import { createBrowserRouter } from 'react-router-dom'
-import App from '../App'
-import Home from '../pages/Home'
-import Login from '../pages/Login'
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
 
-import SignUp from '../pages/SignUp'
-import AdminPanel from '../pages/AdminPanel'
-import AllUsers from '../pages/AllUsers'
+import SignUp from "../pages/SignUp";
+import AdminPanel from "../pages/AdminPanel";
+import AllUsers from "../pages/AllUsers";
 
-import CategoryProduct from '../pages/CategoryProduct'
-import ProductDetails from '../pages/ProductDetails'
-import Cart from '../pages/Cart'
-import SearchProduct from '../pages/SearchProduct'
-import ForgotPassword from '../pages/ForgotPassword'
-import AllProducts from '../pages/AllProduct'
-import UserDetails from '../pages/UserDetails'
+import CategoryProduct from "../pages/CategoryProduct";
+import ProductDetails from "../pages/ProductDetails";
+import Cart from "../pages/Cart";
+import SearchProduct from "../pages/SearchProduct";
+import ForgotPassword from "../pages/ForgotPassword";
+import AllProducts from "../pages/AllProduct";
+import UserDetails from "../pages/UserDetails";
 
 const router = createBrowserRouter([
-    {
-        path : "/",
-        element : <App/>,
-        children : [
-            {
-                path : "",
-                element : <Home/>
-            },
-            {
-                path : "login",
-                element : <Login/>
-            },
-            {
-                path : "forgot-password",
-                element : <ForgotPassword/>
-            },
-            {
-                path : "sign-up",
-                element : <SignUp/>
-            },
-            {
-                path : "product-category",
-                element : <CategoryProduct/>
-            },
-            {
-                path : "product/:id",
-                element : <ProductDetails/>
-            },
-            {
-                path : 'cart',
-                element : <Cart/>
-            },
-            {
-                path : "search",
-                element : <SearchProduct/>
-            },
-             {
-                        path:"user-details",
-                        element:<UserDetails/>
-                    },
-            {
-                path : "admin-panel",
-                element : <AdminPanel/>,
-                children : [
-                    {
-                        path : "all-users",
-                        element : <AllUsers/>
-                    },
-                    {
-                        path : "all-products",
-                        element : <AllProducts/>
-                    },
-                    {
-        path: "*",
-        element: <Home /> // or your custom 404 page
-      }
-                   
-                ]
-            },
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "forgot-password",
+        element: <ForgotPassword />,
+      },
+      {
+        path: "sign-up",
+        element: <SignUp />,
+      },
+      {
+        path: "product-category",
+        element: <CategoryProduct />,
+      },
+      {
+        path: "product/:id",
+        element: <ProductDetails />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+      {
+        path: "search",
+        element: <SearchProduct />,
+      },
+      {
+        path: "user-details",
+        element: <UserDetails />,
+      },
+      {
+        path: "admin-panel",
+        element: <AdminPanel />,
+        children: [
+          {
+            path: "all-users",
+            element: <AllUsers />,
+          },
+          {
+            path: "all-products",
+            element: <AllProducts />,
+          },
+    
+        ],
+        },
+           {
+            path: "*",
+            element: <Home />, // or your custom 404 page
+          
+      },
+    ],
+  },
+]);
 
-
-export default router
+export default router;
